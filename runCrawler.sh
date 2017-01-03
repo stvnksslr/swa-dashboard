107a2df682e7161784b5b3179a5d592aca53789d
Steven Kessler <stvnksslr@gmail.com>
2017-01-02 22:27:51 -0500

    feat(demo script): worked out a good date range and format for a demo script

---
+++ runCrawler.sh
@@ -0,0 +1,2 @@
+# working example script
+./index.js --from 'LGA' --to 'LAX' --leave-date '01/16/2017' --return-date '01/19/2017' --individual-deal-price 50 --passengers 1 --total-deal-price 150 --interval 0.1
